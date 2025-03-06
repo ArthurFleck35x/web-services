56e1cd298ef315c649990daf5c581fb6e4d9e523
ArthurFleck35x <liedtkeleaceline@outlook.com>
2025-03-06 10:26:38 +0100

    styles müssen weiter angepasst werden

--- vue-project/src/components/Footer.vue
+++ vue-project/src/components/Footer.vue
@@ -4,24 +4,24 @@
             <div class="row">
                 <!--Logo und Beschreibung-->
                 <div>
-                    <h5 class="fw-bold">Mein Unternehmen</h5>
+                    <h5 class="title">UniPlace</h5>
                     <p class="text-secondary">
-                        Werde Teil unserer Community!
+                        be part of our community!
                     </p>
                 </div>
                 <!--Navigation-->
                 <div class="col-md-4">
                     <h5 class="fw-bold">Navigation</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="text-secondary text-decoration-none">Startseite</a></li>
-                        <li><a href="#" class="text-secondary text-decoration-none">Über uns</a></li>
-                        <li><a href="#" class="text-secondary text-decoration-none">Leistungen</a></li>
+                        <li><a href="#" class="text-secondary text-decoration-none">Home</a></li>
+                        <li><a href="#" class="text-secondary text-decoration-none">About Us</a></li>
+                        <li><a href="#" class="text-secondary text-decoration-none">Fast FAQ</a></li>
                         <li><a href="#" class="text-secondary text-decoration-none">Kontakt</a></li>
                     </ul>
                 </div>
                 <!--Copyright-->
                 <div class="text-center mt-4 border-top pt-3">
-                    <p class="text-secondary">© {{ currentYear }} UniMarket</p>
+                    <p class="text-secondary">© {{ currentYear }} UniPlace</p>
                 </div>
             </div>
         </div>
@@ -34,10 +34,30 @@ const currentYear = ref(new Date().getFullYear());
 </script>
 
 <style scoped>
-a {
+.footer {
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    color: white;
+    position: relative;
+}
+/* Logo */
+.logo img {
+    height: 30px;
+    width: 30px;
+}
+.title {
+    font-size: 1.5rem; /* Entspricht "text-2xl" in Tailwind */
+    font-weight: bold;
+    background: linear-gradient(to right, #22d3ee, #9333ea); /* Farbverlauf von Cyan nach Lila */
+    -webkit-background-clip: text; /* Hintergrundclip für den Text */
+    color: transparent; /* Text transparent machen, damit der Hintergrund sichtbar wird */
+}
+/*a {
     transition: color 0.3s;
 }
 a:hover {
     color: #f8f9fa !important;
 }
+*/
 </style>
