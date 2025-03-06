8069e942bffede9889e8e53c9e803befdb1b1f92
ArthurFleck35x <liedtkeleaceline@outlook.com>
2025-03-06 12:00:30 +0100

    FAQ page updated

--- vue-project/src/views/FAQ.vue
+++ vue-project/src/views/FAQ.vue
@@ -1,3 +1,38 @@
 <template>
-    
+    <div>
+        <div>
+            <!-- About Us Banner img -->
+            <div>
+                <div>
+                    <h1>
+                        <span>About UniPlace</span>
+                    </h1>
+                    <p>
+                        Pioneering of asset trading across the multiverse
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main>
+        <div>
+            <div>
+                <h2>FAQ </h2>
+                <h3>General</h3>
+                <div>
+                    <span>
+                        Question
+                    </span>
+                    <span>
+                        Answer
+                    </span>
+                </div>
+                <!--für die anderen anpassen-->
+                <h3>Account</h3>
+                <h3>Marketplace</h3>
+                <h3>Support</h3>
+            </div>
+        </div>
+    </main>
 </template>
