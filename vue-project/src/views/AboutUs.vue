4abf0126457839f7be051b6d2c6930691610fbfa
ArthurFleck35x <liedtkeleaceline@outlook.com>
2025-03-06 11:41:24 +0100

    create new files for the views of the project

---
+++ vue-project/src/views/AboutUs.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div>
+            <!-- About Us Banner img -->
+            <div>
+                <div>
+                    <h1>
+                        <span>About UniPlace</span>
+                    </h1>
+                    <p>
+                        Pioneering of asset trading across the multiverse
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main>
+        <section>
+            <div>
+                <div>
+                    <Zap size={20}></Zap>
+                    <h2>
+                    </h2>
+                </div>
+                <h3>
+                    Since 2025
+                </h3>
+                <p>
+                    UniPlace is a platform for trading assets across the multiverse. We are the first to offer this service and have been doing so since 2025. Our platform is secure and easy to use. We offer a wide range of assets to trade, including stocks, bonds, and cryptocurrencies. Our platform is designed to be user-friendly and intuitive, so you can start trading right away. Join us today and start trading assets across the multiverse!</p>
+                <p>next pragraph</p>
+
+            </div>
+        </section>
+
+        <section>
+            <div>
+                <Target size={20}></Target>
+                <h2>Revolutionizing Digital Asset Trading</h2>
+            </div>
+        </section>
+    </main>
+</template>
