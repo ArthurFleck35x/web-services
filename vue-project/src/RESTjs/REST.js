39bf29497c32e8840bb8d0804e72820f2fc016d2
Mortalium <152898932+Mortalium@users.noreply.github.com>
2025-03-17 12:03:46 +0100

    fundament für die REST schnittstellen geschaffen

--- vue-project/src/RESTjs/REST.js
+++ vue-project/src/RESTjs/REST.js
@@ -1,6 +1,10 @@
-const serverURL = ""
+const serverURL = "";
 
-var userID
+var userID;
+
+var currencyRate;
+
+var flagURL;
 
 export async function fetchArticles() {
     try {
@@ -18,7 +22,7 @@ export async function fetchArticles() {
 export async function fetchSearchArticles(searchstring) {
     try {
       const response = await fetch(serverURL+"/searcharticles",{
-        method: "POST",
+        method: "GET",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(searchstring),
       }); // Beispiel-API
@@ -36,7 +40,7 @@ export async function fetchSearchArticles(searchstring) {
 export async function fetchMyArticles() {
     try {
       const response = await fetch(serverURL+"/myarticles",{
-        method: "POST",
+        method: "GET",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(userID),
       }); // Beispiel-API
@@ -48,4 +52,125 @@ export async function fetchMyArticles() {
       console.error('Fehler:', error);
       return []; // Rückgabe einer leeren Liste im Fehlerfall
     }
+}
+
+export async function checkLoginData() {
+  try {
+    const response = await fetch(serverURL+"/login",{
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+    
+
+
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function registerUser() {
+  try {
+    const response = await fetch(serverURL+"/register",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+    
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function fetchCurrencyRate(currency) {
+  try {
+    const response = await fetch(serverURL+"/currency",{
+      method: "GET",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(currency),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function fetchFlagURL(country) {
+  try {
+    const response = await fetch(serverURL+"/flag",{
+      method: "GET",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(country),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function createNewArticle() {
+  try {
+    const response = await fetch(serverURL+"/newarticle",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+    
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function updateArticle() {
+  try {
+    const response = await fetch(serverURL+"/updatearticle",{
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
+}
+
+export async function deleteArticle() {
+  try {
+    const response = await fetch(serverURL+"/deletearticle",{
+      method: "DELETE",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(),
+    }); // Beispiel-API
+    if (!response.ok) throw new Error('Fehler beim Abrufen der Daten');
+
+    const data = await response.json(); // JSON-Daten extrahieren
+
+  } catch (error) {
+    console.error('Fehler:', error);
+    return []; // Rückgabe einer leeren Liste im Fehlerfall
+  }
 }
